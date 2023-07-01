79ad377addad1dc57373964d2794b31133afcd09
Nune Indresh <138300465+nuneindresh143@users.noreply.github.com>
2023-07-01 17:57:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let outputscreen=document.getElementById('output-screen');
+function display(num)
+{
+    outputscreen.value += num;
+}
+function Calculate(){
+    try{
+        outputscreen.value=eval(outputscreen.value);
+    }
+    catch(err)
+    {
+        alert("invalid number");
+    }
+}
+function Clear()
+{
+    outputscreen.value="";
+}
+function del(){
+    outputscreen.value=outputscreen.value.slice(0,-1);
+}
